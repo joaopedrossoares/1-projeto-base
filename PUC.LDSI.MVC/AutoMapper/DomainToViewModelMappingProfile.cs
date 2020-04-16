b9d9f07b74436bb87dacedbd8a06497b21c743ab
Rafael <rafaelangelowow@gmail.com>
2020-04-15 22:00:59 -0300

    subindo bin obj

---
+++ PUC.LDSI.MVC/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using PUC.LDSI.Domain.Entities;
+using PUC.LDSI.MVC.AutoMapper;
+using PUC.LDSI.MVC.Models;
+
+namespace PUC.LDSI.MVC.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Turma, TurmaViewModel>().ReverseMap();
+
+            CreateMap<OpcaoAvaliacao, OpcaoAvaliacaoViewModel>().ReverseMap();
+            CreateMap<QuestaoAvaliacao, QuestaoAvaliacaoViewModel>().ReverseMap();
+            CreateMap<Avaliacao, AvaliacaoViewModel>()
+    .ForMember(destino => destino.Professor, opt => opt.MapFrom(avaliacao => avaliacao.Professor.Nome))
+    .ReverseMap();
+        }
+    }
+}
---
+++ PUC.LDSI.MVC/Models/QuestaoAvaliacaoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PUC.LDSI.MVC.Models
+{
+    public class QuestaoAvaliacaoViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int AvaliacaoId { get; set; }
+
+        [Required(ErrorMessage = "O Tipo é requerido.")]
+        public int Tipo { get; set; }
+
+        [Required(ErrorMessage = "O Enunciado é requerido.")]
+        [MaxLength(225, ErrorMessage = "Informe no maximo {0} caracteres.")]
+        public string Enunciado { get; set; }
+
+        public AvaliacaoViewModel Avaliacao { get; set; }
+
+        public List<OpcaoAvaliacaoViewModel> OpcaoAvaliacao { get; set; }
+
+        /* TODO Conterá uma lógica de validação da opção da avaliação;
+Se tiver erro na validação seu texto será a descrição do erro ou pendência.
+Se tiver menos de 4 opções o texto deve ser "A questão deve ter pelo menos 4 (quatro) opções."
+Se for questão de multipla escolha e não tiver nenhuma opção verdadeira: "A questão deve ter pelo menos 1 (uma) opção verdadeira."
+Qualquer outro caso o texto será uma string vazia.*/
+        public string Erro { get; set; }
+    }
+}
